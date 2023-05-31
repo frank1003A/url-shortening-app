bf3f7d21e75f871b3df19fb3a9a18804b2283ea9
Frank Ezene <63779386+frank1003A@users.noreply.github.com>
2023-05-31 22:12:05 +0100

    update Header.tsx

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,9 +2,15 @@ import logo from "../../images/logo.svg";
 import Button from "../buttons/Button";
 import IconButton from "../buttons/Iconbutton";
 
+import { useState } from "react";
 import ham from "../../images/burger-menu-svgrepo-com.svg";
+import Menu from "../menu/Menu";
+import MenuButton from "../menu/MenuButton";
+import MenuList from "../menu/MenuList";
+import MenuOption from "../menu/MenuOption";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   return (
     <header>
       <div className="header-main">
@@ -12,9 +18,13 @@ const Header = () => {
         <img src={logo} alt="main-logo" />
         {/** Navbar */}
         <nav>
-          <span>Features</span>
-          <span>Pricing</span>
-          <span>Resources</span>
+          <a href="https://github.com/frank1003A/url-shortening-app">
+            Features
+          </a>
+          <a href="https://github.com/frank1003A/url-shortening-app">Pricing</a>
+          <a href="https://github.com/frank1003A/url-shortening-app">
+            Resources
+          </a>
         </nav>
       </div>
       {/**action buttons */}
@@ -25,9 +35,24 @@ const Header = () => {
         <Button className="signup-btn" type="rounded">
           Sign Up
         </Button>
-        <IconButton className="hamburger-icon">
-          <img src={ham} alt="hamburger-icon" />
-        </IconButton>
+        <Menu>
+          <MenuButton>
+            <IconButton
+              className="hamburger-icon"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              <img src={ham} alt="hamburger-icon" />
+            </IconButton>
+          </MenuButton>
+          <MenuList handler={showMenu}>
+            <MenuOption>Features</MenuOption>
+            <MenuOption>Pricing</MenuOption>
+            <MenuOption>Resources</MenuOption>
+            <hr />
+            <MenuOption>Login</MenuOption>
+            <Button type="rounded">Sign Up</Button>
+          </MenuList>
+        </Menu>
       </div>
     </header>
   );
